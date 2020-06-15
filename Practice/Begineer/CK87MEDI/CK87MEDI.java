2ae5d9e9a894e084be49e29a2919f25cc8274151
Ritesh Khadse <48131636+Ritsz123@users.noreply.github.com>
2020-06-15 21:29:36 +0530

    15 june 2020

---
+++ Practice/Begineer/CK87MEDI/CK87MEDI.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.Arrays;
+class TEMPLELA {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-->0) {
+            String[] line= br.readLine().split(" ");
+            int n = Integer.parseInt(line[0]);
+            int k = Integer.parseInt(line[1]);
+            String[] line2 = br.readLine().split(" ");
+            int [] nos = new int[n];
+            for (int i = 0; i < n; i++) {
+                nos[i]= Integer.parseInt(line2[i]);
+            }
+            Arrays.sort(nos);
+            int median = (n+k)/2;
+            System.out.println(nos[median]);
+        }
+    }
+}
---
+++ Practice/Begineer/UTMOPR/UTMOPR.java
@@ -0,0 +1,27 @@
+import java.io.*;
+class TEMPLELA {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-->0) {
+            String[] line = br.readLine().split(" ");
+            int n = Integer.parseInt(line[0]);
+            int k = Integer.parseInt(line[1]);
+            String[] line2 = br.readLine().split(" ");
+            int [] nos = new int[n];
+            int sum=0;
+            for(int i=0;i<n;i++){
+                sum+= Integer.parseInt(line2[i]);
+            }
+            int num =sum+1;
+            for (int i = 1; i < k; i++) {
+                sum+=num;
+                num=sum+1;
+            }
+            if(num%2==1) System.out.println("odd");
+            else System.out.println("even");
+            
+            
+        }
+    }
+}
